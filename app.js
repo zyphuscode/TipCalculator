951becd20f2b4868a6b326c4ac501f2ed6daf53a
Zehra Nisar <zyphuscode@gmail.com>
2020-09-15 22:19:38 +1000

    Created a tip form calculator.

---
+++ app.js
@@ -0,0 +1,32 @@
+const bill = document.getElementById("bill-money");
+const customerNumber = document.getElementById("customer");
+const rateEL = document.getElementById("rate");
+const calcButton = document.getElementById("calc");
+const resultDisplay = document.getElementById("result");
+const tipPercent = document.getElementById("tipPercent");
+
+
+//function to calculate the tip
+function calculateTip(){
+  const billInput = bill.value;
+  console.log(billInput);
+  const customerInput = customerNumber.value;
+  console.log(customerInput);
+
+  const tipInput =  tipPercent.value;
+  console.log(tipInput);
+  const newSum = (tipInput + billInput);
+
+  rateEL.innerText = `Your Total Tip According to your bill ${billInput} and ${customerInput} guests`
+  resultDisplay.innerText = (newSum/customerInput).toFixed(2);
+
+}
+
+
+//adding event Listeners
+bill.addEventListener("change", calculateTip);
+customerNumber.addEventListener("change", calculateTip);
+calcButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  calculateTip();
+});
