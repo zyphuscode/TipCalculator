4a0d125266cd5a3ccc9c7487acc9c762d2e30e26
Zehra Nisar <zyphuscode@gmail.com>
2020-09-16 12:20:56 +1000

    add multiple divs to dispaly the results.

--- app.js
+++ app.js
@@ -1,33 +1,26 @@
 const bill = document.getElementById("bill-money");
 const customerNumber = document.getElementById("customer");
-const rateEL = document.getElementById("rate");
 const calcButton = document.getElementById("calc");
-const resultDisplay = document.getElementById("result");
 const tipPercent = document.getElementById("tipPercent");
-const loader = document.getElementById("loader");
 
 //function to calculate the tip
 function calculateTip(){
-  const billInput = bill.value;
-  console.log(billInput);
-  const customerInput = customerNumber.value;
-  console.log(customerInput); 
+  const billInput = parseInt(bill.value);
+  const customerInput = parseInt(customerNumber.value);
+  const tipInput =  parseInt(tipPercent.value);
+  const totalAmount = billInput+(billInput*tipInput/100)
+  const perGuestAmount = totalAmount/customerInput;
 
-  const tipInput =  tipPercent.value;
-  console.log(tipInput);
-  const newSum = (tipInput + billInput) / 100;
-
-
-  
-  rateEL.innerText = `Your Total Tip According to your bill  $${billInput} and ${customerInput} guests is`
-  resultDisplay.innerText = "$" + (newSum/customerInput).toFixed(2);
+  document.getElementById("billAmount").innerText = `Total bill: ${billInput}`
+  document.getElementById("guestAmount").innerText = `Total guests: ${customerInput}`
+  document.getElementById("totalBillAmount").innerText = `Total bill including tip: ${totalAmount}`
+  document.getElementById("perGuestAmount").innerText = `Total for each guest is: ${perGuestAmount}`
 }
-
-
 
 //adding event Listeners
 bill.addEventListener("change", calculateTip);
 customerNumber.addEventListener("change", calculateTip);
+tipPercent.addEventListener("change",calculateTip)
 calcButton.addEventListener("click", (e) => {
   e.preventDefault();
   calculateTip();
