43d7e5969cebe891d205e02d97d42793189a9e03
Zehra Nisar <zyphuscode@gmail.com>
2020-09-16 11:06:31 +1000

    added icons and remove the value

--- app.js
+++ app.js
@@ -4,25 +4,25 @@ const rateEL = document.getElementById("rate");
 const calcButton = document.getElementById("calc");
 const resultDisplay = document.getElementById("result");
 const tipPercent = document.getElementById("tipPercent");
-
+const loader = document.getElementById("loader");
 
 //function to calculate the tip
 function calculateTip(){
   const billInput = bill.value;
   console.log(billInput);
   const customerInput = customerNumber.value;
-  console.log(customerInput);
+  console.log(customerInput); 
 
   const tipInput =  tipPercent.value;
   console.log(tipInput);
   const newSum = (tipInput + billInput) / 100;
 
+
+  
   rateEL.innerText = `Your Total Tip According to your bill  $${billInput} and ${customerInput} guests is`
   resultDisplay.innerText = "$" + (newSum/customerInput).toFixed(2);
-
 }
 
-//format money
 
 
 //adding event Listeners
